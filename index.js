11639e975b651a69a80e68f87ad3332de0976879
Igor <igormartins.all@gmail.com>
2024-09-22 20:02:45 -0300

    conditionals

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const student01 = 'Mayk'
-const student02 = 'Diego'
-const student03 = 'Martins'
-
 const grade01 = 9.8
+
+const student02 = 'Diego'
 const grade02 = 10
+
+const student03 = 'Martins'
 const grade03 = 6
 
 const average = (grade01 + grade02 + grade03) / 3
 
-console.log(average) // 8.6
+if(average > 5) {
+  console.log(`Class average: ${average} | Approved class!`)
+} else {
+  console.log(`Class average: ${average} | Failed class!`)
+}
