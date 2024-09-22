d196e03c08c86eb664208195699b0bab1d03123a
Igor <igormartins.all@gmail.com>
2024-09-22 18:43:33 -0300

    doing calculations with javascript

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-// Commentary
-
+const student01 = 'Mayk'
 const student02 = 'Diego'
-const student01 = "Mayk"
+const student03 = 'Martins'
 
 const grade01 = 9.8
 const grade02 = 10
+const grade03 = 6
+
+const average = (grade01 + grade02 + grade03) / 3
+
+console.log(average) // 8.6
