576859b90b9f13bb03d2ba5064fc04cc72162e60
Igor <igormartins.all@gmail.com>
2024-09-23 09:38:25 -0300

    retirement

--- challenges/retirement.js
+++ challenges/retirement.js
@@ -7,8 +7,8 @@ if (((sex === "M") && (contribution >= 35)) || ((sex === "F") && (contribution >
   if (((sex === "M") && ((contribution + age) >= 95)) || ((sex === "F") && ((contribution + age) >= 85))) {
     console.log(`${name}, you can retire!`)
   } else {
-    console.log(`${name}, you can't retire!`)
+    console.log(`${name}, you can't retire yet!`)
   }
 } else {
-  console.log(`${name}, you can't retire!`)
+  console.log(`${name}, you can't retire yet!`)
 }
