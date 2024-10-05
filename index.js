a6a498a76490abff645c96dd4ef332e74e2676c2
Igor <igormartins.all@gmail.com>
2024-10-05 16:23:13 -0300

    array

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const student01 = {
-  name: 'Mayk',
-  grade: 9.8
-}
+const students = [
+  {
+    name: 'Mayk',
+    grade: 9.8
+  },
+  {
+    name: 'Diego',
+    grade: 10
+  },
+  {
+    name: 'Martins',
+    grade: 6
+  }
+]
 
-const student02 = {
-  name: 'Diego',
-  grade: 10
-}
-
-const student03 = {
-  name: 'Martins',
-  grade: 6
-}
-
-const average = (student01.grade + student02.grade + student03.grade) / 3
+const average = (students[0].grade + students[1].grade + students[2].grade) / 3
 
 if(average > 5) {
   console.log(`Class average: ${average} | Approved class!`)
